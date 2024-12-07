8983e95a918725dbfb2040a48db7fb9dca2cec30
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2024-12-07 20:08:03 +0100

    feat(routes): add basic screens
    Add basic Home, LogMood and Profile screens.

---
+++ src/routes/LogMoodScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const LogMoodScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Log Mood Screen</Text>
+    </View>
+  );
+};
+
+export default LogMoodScreen;
