2d4323b313205dd57e932ef0ae2db28d5f1f1740
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2024-12-15 18:33:54 +0100

    refactor(App): setup StyleSheet
    Setup Unistyle StyleSheet configuration.

--- App.tsx
+++ App.tsx
@@ -8,6 +8,18 @@
 import React from 'react';
 import {Navigation} from './src/navigation';
 
+import {StyleSheet} from 'react-native-unistyles';
+import theme from './src/styles/theme';
+
+StyleSheet.configure({
+  themes: {
+    light: theme,
+  },
+  settings: {
+    initialTheme: 'light',
+  },
+});
+
 const App = () => {
   return <Navigation />;
 };
