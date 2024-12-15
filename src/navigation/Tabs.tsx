f9ee00efda2d01e326d85017b8b86418f98181ee
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2024-12-15 18:35:13 +0100

    refactor(Tabs): update styles
    Update Bottom TabBar styles: custom icons, background color, etc.

--- src/navigation/Tabs.tsx
+++ src/navigation/Tabs.tsx
@@ -2,12 +2,48 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../routes/HomeScreen';
 import LogMoodScreen from '../routes/LogMoodScreen';
 import ProfileScreen from '../routes/ProfileScreen';
+import {HomeIcon, MoodIcon, ProfileIcon} from '../../assets';
+import TabBarIcon from './components/TabBarIcon';
+import {StyleSheet} from 'react-native-unistyles';
+
+const styles = StyleSheet.create(theme => ({
+  tabBarBackgroundColor: {
+    color: theme.colors.common.primary,
+  },
+}));
 
 const BottomTabs = createBottomTabNavigator({
   screens: {
-    Home: HomeScreen,
-    LogMood: LogMoodScreen,
-    Profile: ProfileScreen,
+    Home: {
+      screen: HomeScreen,
+      options: {
+        tabBarIcon: props => (
+          <TabBarIcon Icon={HomeIcon} focused={props.focused} />
+        ),
+      },
+    },
+    LogMood: {
+      screen: LogMoodScreen,
+      options: {
+        tabBarIcon: props => (
+          <TabBarIcon Icon={MoodIcon} focused={props.focused} />
+        ),
+      },
+    },
+    Profile: {
+      screen: ProfileScreen,
+      options: {
+        tabBarIcon: props => (
+          <TabBarIcon Icon={ProfileIcon} focused={props.focused} />
+        ),
+      },
+    },
+  },
+  screenOptions: {
+    tabBarInactiveBackgroundColor: styles.tabBarBackgroundColor.color,
+    tabBarActiveBackgroundColor: styles.tabBarBackgroundColor.color,
+    tabBarShowLabel: false,
+    headerShown: false,
   },
 });
 
