b9e046dd1575b4e3de5c4748ded52aedefcca04e
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2024-12-15 11:05:51 +0100

    refactor(HomeScreen): show latest mood
    Show the latest mood that the user added.

--- src/routes/HomeScreen.tsx
+++ src/routes/HomeScreen.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import useStorage from '../state/local/hooks/useStorage';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+  },
+  moodText: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+});
 
 const HomeScreen = () => {
+  const {storage} = useStorage();
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+    <View style={styles.wrapper}>
+      {storage?.moods[0]?.type && (
+        <Text style={styles.moodText}>
+          Latest Mood: {JSON.stringify(storage.moods[0].type, null, 2)}
+        </Text>
+      )}
     </View>
   );
 };
