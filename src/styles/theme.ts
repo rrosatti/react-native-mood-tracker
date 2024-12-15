0fe14e2dfa695cf328128225cd757fdf0a748b48
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2024-12-15 18:33:09 +0100

    feat(styles): add theme config
    Add initial theme config.

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+const theme = {
+  colors: {
+    common: {
+      primary: '#A4031F',
+      background: '#EDE7D9',
+    },
+    moods: {
+      sad: '#f93943',
+      neutral: '#294c60',
+      happy: '#379634',
+    },
+    progressBar: {
+      minTrack: '#294c60',
+      maxTrack: '#adb6c4',
+    },
+  },
+};
+
+export default theme;
