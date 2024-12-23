d6f3e6ef1e3345a9522cba4ad1b8c09ac4a8eb16
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2024-12-23 15:44:58 +0100

    refactor(routes/ProfileScreen): show language picker
    Show language picker component.

--- src/routes/ProfileScreen.tsx
+++ src/routes/ProfileScreen.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {StyleSheet} from 'react-native-unistyles';
+import {useTranslation} from 'react-i18next';
+
+import LanguagePicker from '../components/LanguagePicker';
 
 const styles = StyleSheet.create((theme, miniRuntime) => ({
   wrapper: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     paddingHorizontal: 16,
     paddingTop: miniRuntime.insets.top + 20,
     paddingBottom: miniRuntime.insets.bottom + 20,
     backgroundColor: theme.colors.common.background,
   },
+  languageSelectionTitle: {fontSize: 24, fontWeight: 'bold'},
 }));
 
 const ProfileScreen = () => {
+  const {t} = useTranslation('common');
   return (
     <View style={styles.wrapper}>
-      <Text>Profile Screen</Text>
+      <Text style={styles.languageSelectionTitle}>
+        {t('profile.languageSelectorTitle')}
+      </Text>
+      <LanguagePicker />
     </View>
   );
 };
